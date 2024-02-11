556dc33f82ea999faaf3372d4d4d62a36ac43f03
Eli <wer0222@naver.com>
2024-02-11 18:28:08 +0900

    #81 사진첨부 버튼 및 섹션 타이틀 UI 구현

--- Cheffi/Presentation/RestaurantRegistScene/RestaurantInfoCompose/RestaurantInfoComposeReducer.swift
+++ Cheffi/Presentation/RestaurantRegistScene/RestaurantInfoCompose/RestaurantInfoComposeReducer.swift
@@ -10,21 +10,52 @@ import Combine
 import ComposableArchitecture
 
 struct RestaurantInfoComposeReducer: Reducer {
+    let useCase: RestaurantUseCase
     let steps: PassthroughSubject<RouteStep, Never>
 
     init(
+        useCase: RestaurantUseCase,
         steps: PassthroughSubject<RouteStep, Never>
     ) {
+        self.useCase = useCase
         self.steps = steps
     }
     
     struct State: Equatable {
         var restaurant: RestaurantInfoDTO
+        
+        let navigationBarState = NavigationBarReducer.State(
+            title: "내 맛집 등록",
+            buttonKind: .back
+        )
+        var bottomButtonState = BottomButtonReducer.State(
+            title: "다음",
+            able: false
+        )
     }
     
-    enum Action {}
+    enum Action {
+        case navigaionBarAction(NavigationBarReducer.Action)
+        case bottomButtonAction(BottomButtonReducer.Action)
+    }
     
-    func reduce(into state: inout State, action: Action) -> Effect<Action> {}
+    func reduce(into state: inout State, action: Action) -> Effect<Action> {
+        switch action {
+        case .navigaionBarAction(let action):
+            switch action {
+            case .tap:
+                steps.send(.popToNavigationController)
+                return .none
+            }
+        case .bottomButtonAction(let action):
+            switch action {
+            case .tap:
+                // TODO: Eli - 맛집정보 해시태그 화면으로 이동
+                // steps.send(.hashtag....)
+                return .none
+            }
+        }
+    }
 }
 
 extension RestaurantInfoComposeReducer: Stepper {}
--- Cheffi/Presentation/RestaurantRegistScene/RestaurantInfoCompose/RestaurantInfoComposeView.swift
+++ Cheffi/Presentation/RestaurantRegistScene/RestaurantInfoCompose/RestaurantInfoComposeView.swift
@@ -12,8 +12,107 @@ import ViewStore
 
 @ViewStore(RestaurantInfoComposeReducer.self)
 struct RestaurantInfoComposeView: View {
+    private enum Metrics {
+        static let safeAreaPadding = 16.0
+        static let headlineTextTopPadding = 32.0
+        static let headlineTextHorizontalSpacing = 8.0
+        static let photoThumbnailImageSize = CGSize(width: 88.0, height: 88.0)
+        static let attatchPhotoButtonContentsSpacing = 4.0
+        static let attatchPhotoButtonViewPadding = 16.0
+        static let attatchPhotoButtonPadding = 8.0
+        static let attatchPhotoButtonCornerRadius = 8.0
+    }
+    
     var body: some View {
-        Text(viewStore.restaurant.name)
+        ZStack {
+            VStack(spacing: 0) {
+                NavigationBarView(store.scope(
+                    state: \.navigationBarState,
+                    action: RestaurantInfoComposeReducer.Action.navigaionBarAction
+                ))
+                
+                // 사진첨부 영역
+                VStack(spacing: 0) {
+                    HStack(spacing: 0) {
+                        Text("맛집에서의 경험은 어떠셨나요?")
+                            .font(.custom("SUIT", size: 20).weight(.semibold))
+                            .minimumScaleFactor(0.5)
+                            .foregroundColor(.cheffiGray8)
+                        
+                        Spacer(minLength: Metrics.headlineTextHorizontalSpacing)
+                        
+                        Text("3개이상 등록")
+                            .font(.custom("SUIT", size: 14))
+                            .minimumScaleFactor(0.5)
+                            .foregroundColor(.cheffiGray5)
+                    }
+                    .padding(.top, Metrics.headlineTextTopPadding)
+                    
+                    // TODO: Eli - 첨부된 사진들이 있다면..
+                    LazyHStack {
+                        
+                    }
+                    .frame(
+                        width: .infinity,
+                        height: Metrics.photoThumbnailImageSize.height
+                    )
+                    
+                    Button {
+                        
+                    } label: {
+                        HStack(spacing: Metrics.attatchPhotoButtonContentsSpacing) {
+                            Image(.attatchPhoto)
+                            
+                            Text("사진 첨부하기")
+                                .font(.custom("SUIT", size: 14).weight(.medium))
+                        }
+                        .padding(Metrics.attatchPhotoButtonPadding)
+                        .frame(maxWidth: .infinity)
+                    }
+                    .overlay(
+                        RoundedRectangle(cornerRadius: Metrics.attatchPhotoButtonCornerRadius)
+                            .inset(by: 0.5)
+                            .stroke(
+                                .cheffiPink1,
+                                lineWidth: 1
+                            )
+                    )
+                    .padding(.vertical, Metrics.attatchPhotoButtonViewPadding)
+                    .foregroundColor(.mainCTA)
+                } // 사진첨부 영역
+                
+                // 리뷰작성 영역
+                VStack(spacing: 0) {
+                    Text("어떤 메뉴를 드셨나요?")
+                        .font(.custom("SUIT", size: 20).weight(.semibold))
+                        .foregroundColor(.cheffiGray8)
+                        .padding(.top, Metrics.headlineTextTopPadding)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                    
+                } // 리뷰작성 영역
+                
+                // 메뉴선택 영역
+                VStack(spacing: 0) {
+                    
+                } // 메뉴선택 영역
+                
+                Spacer()
+                
+                BottomButtonView(store.scope(
+                    state: \.bottomButtonState,
+                    action: RestaurantInfoComposeReducer.Action.bottomButtonAction
+                ))
+            }
+            .padding(.horizontal, Metrics.safeAreaPadding)
+            
+//            if viewStore.isShowConfirmPopup {
+//                ConfirmPopupView(store.scope(
+//                    state: \.confirmPopupState,
+//                    action: RestaurantRegistComposeReducer.Action.confirmPopupAction
+//                ))
+//            }
+        }
+//        .animation(.default, value: viewStore.isShowConfirmPopup)
     }
 }
 
@@ -35,7 +134,10 @@ struct RestaurantInfoComposeView_Preview: PreviewProvider {
                     registered: false
                 )
             )) {
-                RestaurantInfoComposeReducer(steps: PassthroughSubject<RouteStep, Never>())._printChanges()
+                RestaurantInfoComposeReducer(
+                    useCase: PreviewRestaurantRegistUseCase(),
+                    steps: PassthroughSubject<RouteStep, Never>()
+                )._printChanges()
             }
         )
     }
