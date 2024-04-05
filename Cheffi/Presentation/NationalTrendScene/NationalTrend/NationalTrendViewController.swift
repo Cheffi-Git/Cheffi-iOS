384985246911c5952c0b6bc1741fb47e4908fb7c
Eli <wer0222@naver.com>
2024-04-05 17:05:12 +0900

    #100 호스팅 뷰컨트롤러 추가

--- Cheffi/Presentation/NationalTrendScene/NationalTrend/NationalTrendViewController.swift
+++ Cheffi/Presentation/NationalTrendScene/NationalTrend/NationalTrendViewController.swift
@@ -6,17 +6,29 @@
 //
 
 import UIKit
+import ComposableArchitecture
 
 final class NationalTrendViewController: UIViewController {
-    static func instance<T: NationalTrendViewController>() -> T {
+    static func instance<T: NationalTrendViewController>(reducer: NationalTrendReducer) -> T {
         let vc: T = .instance(storyboardName: .nationalTrend)
+        vc.reducer = reducer
         return vc
     }
+    
+    private var reducer: NationalTrendReducer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         debugPrint("------------------------------------------")
         debugPrint("NationalTrendViewController viewDidLoad")
         debugPrint("------------------------------------------")
+        
+        addHostingController(
+            view: NationalTrendView(
+                Store(initialState: NationalTrendReducer.State()) {
+                    reducer._printChanges()
+                }
+            )
+        )
     }
 }
